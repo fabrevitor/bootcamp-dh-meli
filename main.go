deab9f5f9908d092aeded5a3baca03e959560841
Vitor Fabre de Souza <vitor_fabre@hotmail.com>
2022-05-20 08:40:55 -0300

    Exercicio 1 Aula Tarde 1905

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Ola!")
+	fmt.Println("Ola!")
+	exercicio1905Tarde1()
+}
+
+func exercicio1905Tarde1() {
+	palavra := "Bootcamp"
+	fmt.Println("Tamanho: ", len(palavra))
+
+	for _, r := range palavra {
+		fmt.Println(string(r))
+	}
+
 }
