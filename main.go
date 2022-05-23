d9f5abd789bd6df8aedb5c48be9745cce1966d42
Vitor Fabre de Souza <vitor_fabre@hotmail.com>
2022-05-23 20:54:49 -0300

    Reset arquivos - orgazanizando...

--- main.go
+++ main.go
@@ -3,16 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Ola!")
-	exercicio1905Tarde1()
-}
-
-func exercicio1905Tarde1() {
-	palavra := "Bootcamp"
-	fmt.Println("Tamanho: ", len(palavra))
-
-	for _, r := range palavra {
-		fmt.Println(string(r))
-	}
-
+	fmt.Println("Hello bootcamp!")
 }
