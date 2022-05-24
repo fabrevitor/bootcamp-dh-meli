b5a45738920b7572d86a47aab2bbdad8546c35b5
Vitor Fabre de Souza <vitor_fabre@hotmail.com>
2022-05-24 17:27:03 -0300

    Ajustes tarde - 24-05

--- go-bases-1/tarde/ex1/ex1.go
+++ go-bases-1/tarde/ex1/ex1.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	palavra := "Teste"
+
+	fmt.Println("Tamanho da palavra: ", len(palavra))
+
+	for _, r := range palavra {
+		fmt.Println(string(r))
+	}
 }
